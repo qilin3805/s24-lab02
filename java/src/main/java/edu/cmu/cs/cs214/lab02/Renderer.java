6a0cdcff59c485be1f82fd1feb432a0b34dab545
qilin3805 <147442831+qilin3805@users.noreply.github.com>
2024-01-23 17:37:34 -0500

    Lab02 finished.

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,17 +3,27 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    private Shape shape;
+
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
-
-        // assume implementation
-
+        double area = shape.getArea();
         System.out.println("Shape printed\n" + "Its area is " + area);
     }
 }
+    // public Rectangle rectangle;
+    
+    // Renderer(Rectangle rectangle) {
+    //     this.rectangle = rectangle;
+    // }
+
+    // void draw() {
+    //     double area = rectangle.getArea();
+
+    //     // assume implementation
+
+    //     System.out.println("Shape printed\n" + "Its area is " + area);
+    // }
